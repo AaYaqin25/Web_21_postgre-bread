d2054f48b7a02b3c615a329f1ba23f807a216332
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-11-13 20:03:32 +0700

    check point

--- models/user.js
+++ models/user.js
@@ -1,57 +1,67 @@
 class Manipulation {
 
     static read(db, page, word, link, callback) {
-
+        const sortBy = word.sortBy || 'id'
+        const sortMode = word.sortMode || 'asc'
+        const url = link == '/' ? '/?page=1&sortBy=id&sortMode=asc' : link 
+        let param = [];
         let value = [];
-        const url = link == '/' ? '/?page = 1' : link 
+        let counter = 1;
         const limit = 3
         const offset = (page - 1) * limit
         
         if (word.idch == 'on' && word.id != '') {
-            value.push(`id = ${word.id}`)
+            param.push(`id = $${counter++} `)
+            value.push(`${word.id}`)
         }
 
         if (word.stringch == 'on' && word.string != '') {
-            value.push(`string LIKE '%${word.string}%'`)
+            param.push(`string LIKE '%' || $${counter++} || '%'`)
+            value.push(`${word.string}`)
         }
 
         if (word.integerch == 'on' && word.integer != '') {
-            value.push(`integer = ${word.integer}`)
+            param.push(`integer = $${counter++} `)
+            value.push(`${word.integer}`)
         }
 
         if (word.floatch == 'on' && word.float != '') {
-            value.push(`float = ${word.float}`)
+            param.push(`float = $${counter++} `)
+            value.push(`${word.float}`)
         }
 
         if (word.datech == 'on' && word.startdate != '' && word.enddate != '') {
-            value.push(`date BETWEEN '${word.startdate}' AND '${word.enddate}'`)
+            param.push(`date BETWEEN $${counter++}  AND $${counter++} `)
+            value.push(`${word.startdate}`)
+            value.push(`${word.enddate}`)
         }
 
         if (word.booleanch == 'on' && word.boolean != '') {
-            value.push(`boolean = '${word.boolean}'`)
+            param.push(`boolean = $${counter++} `)
+            value.push(`${word.boolean}`)
         }
 
 
         let syntax = "SELECT COUNT(*) AS total FROM manipulate"
-        if (value.length > 0) {
-            syntax += ` WHERE ${value.join(' AND ')}`
+        if (param.length > 0) {
+            syntax += ` WHERE ${param.join(' AND ')}`
         }
         
-        db.query(syntax, (err, data) => {
+        db.query(syntax, value, (err, data) => {
             if (err) return console.log("gagal", err);
             
             const jumlah = data.rows[0].total
             const jumlahPage = Math.ceil(jumlah / limit)
 
-
             syntax = "SELECT * FROM manipulate"
-            if (value.length > 0) {
-                syntax += ` WHERE ${value.join(' AND ')}`
+            if (param.length > 0) {
+                syntax += ` WHERE ${param.join(' AND ')}`
             } 
-            syntax += ` LIMIT ${limit} OFFSET ${offset}`
 
-            
-            db.query(syntax, (err, data) => {
+            syntax += ` ORDER BY ${sortBy} ${sortMode} LIMIT $${counter++}  OFFSET $${counter++} `
+
+            console.log(syntax);
+            db.query(syntax, [...value, limit, offset], (err, data) => {
                 if (err) return console.log("gagal", err);
                 callback(
                     data.rows,
